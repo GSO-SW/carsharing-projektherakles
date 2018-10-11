f5f368eb40c9a9b1fbd38868ae0469ee4d3f9f4d
Jan Gramm <19171922+CeJayBoehmermann@users.noreply.github.com>
2018-10-11 14:03:12 +0200

    Mainform Name Fix
    Co-Authored-By: Fabian Martens <fabianmartens@users.noreply.github.com>
    Co-Authored-By: Marvin-René <nikamy@users.noreply.github.com>

--- Project Herakles/Project Herakles/Program.cs
+++ Project Herakles/Project Herakles/Program.cs
@@ -16,7 +16,7 @@ namespace Project_Herakles
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
