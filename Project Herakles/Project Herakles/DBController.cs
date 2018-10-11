de7b73e2801fa92f4a5f9d89c3600512b6eb3803
Jan Gramm <19171922+CeJayBoehmermann@users.noreply.github.com>
2018-10-11 13:53:06 +0200

    Datenbank Controller Beispiel Programm
    Co-Authored-By: Fabian Martens <fabianmartens@users.noreply.github.com>

---
+++ Project Herakles/Project Herakles/DBController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Herakles
+{
+    class DBController
+    {
+
+        // set connection string to my local sql server database
+        // can be retrieved from the database connection properties in the Server-Explorer
+        string connectionString = @"host=localhost;user=root;database=carsharingdb";
+        internal void ConnectToLocalDb()
+        {
+            // create SqlConnection object
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // open connection to database
+                    con.Open();
+                    // INTERACTION WITH DATABASE COMES HERE
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    // close connection to database
+                    con.Close();
+                }
+
+            }
+        }
