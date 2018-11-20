c672ba2f2f615237c63e0483b28da125c2a8a476
Jan <Jan@JANSDESKTOPPC>
2018-11-20 17:50:19 +0100

    Verbindung zur datenbank klann hergestellt werden

--- Project Herakles/Project Herakles/DBController.cs
+++ Project Herakles/Project Herakles/DBController.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Project_Herakles
 {
     class DBController
     {
+
+        string connectionString = @"host=localhost;user=root;database=carsharingdb";
+
+        internal void ConnectToDb()
+        {
+            // create SqlConnection object
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Verbindung Hergestellt");
+                    // INTERACTION WITH DATABASE COMES HERE
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    // close connection from database
+                    connection.Close();
+                }
+
+            }
+        }
     }
 }
+
