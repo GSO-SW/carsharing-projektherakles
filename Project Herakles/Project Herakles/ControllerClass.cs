0d4fe1c8f320d0c19e720ff1e8c6563bd6cf3ca6
Jan Gramm <>
2018-12-07 15:29:07 +0100

    Login Fix

--- Project Herakles/Project Herakles/ControllerClass.cs
+++ Project Herakles/Project Herakles/ControllerClass.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Project_Herakles
 {
     class ControllerClass
     {
+        internal bool Login(string loginName, string password)
+        {
+            DBController dBController = new DBController();
+            if (dBController.checkLoginData(loginName, password) == true)
+            {
+                MessageBox.Show("Login erfolgreich");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Login fehlgeschlagen");
+                return false;
+            }
+
+        }
     }
 }
